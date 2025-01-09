3d7e3b2dd8331c003af1423ebb30f4c2ca72a14a
daichi <dadewujing0@gmail.com>
2025-01-09 13:27:15 +0900

    aa

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -45,3 +45,20 @@ $(document).ready(function(){
         slidesToShow: 1,
     });
   });
+
+
+  VANTA.NET({
+    el: "#goal",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xb7d3e8,
+    backgroundColor: 0xffffff,
+    points: 8.00,
+    maxDistance: 16.00,
+    spacing: 13.00
+  })
