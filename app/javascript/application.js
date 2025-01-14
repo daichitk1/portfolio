4310063e40f0f9b554c2cb9b6aec3c80a927ec2f
daichi <dadewujing0@gmail.com>
2025-01-14 16:26:50 +0900

    aa

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -62,19 +62,3 @@ $(document).ready(function(){
     maxDistance: 16.00,
     spacing: 13.00
   })
-
-  VANTA.NET({
-    el: ".top-introduction",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    scale: 1.00,
-    scaleMobile: 1.00,
-    color: 0xb7d3e8,
-    backgroundColor: 0xffffff,
-    points: 8.00,
-    maxDistance: 16.00,
-    spacing: 13.00
-  })
